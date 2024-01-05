2b8b093919073c2d8103827d9b64375a01598234
= <=>
2024-01-05 16:53:34 -0600

    added Home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./Header.jsx";
+import Home from "../pages/Home.jsx";
 
 export default function App() {
   return (
     <div className="w-full lg:w-4/5 m-auto">
       <Header />
-      <main></main>
+      <main>
+        <Routes>
+          <Route index element={<Home />}></Route>
+        </Routes>
+      </main>
     </div>
   );
 }

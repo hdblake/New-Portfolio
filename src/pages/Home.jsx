bbd83e3ce93b9aba41371b723327987143cf8616
= <=>
2024-01-05 17:58:33 -0600

    added contact and resume button/links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
+import resume from "../images/Resume.pdf";
+
 export default function Home() {
   return (
-    <section className="p-2">
+    <section className="p-4">
       <h1 className="text-[42px] md:text-5xl font-header text-accent text-center md:text-left">
         Hi, I&apos;m{" "}
         <span className="text-primary underline decoration-4 decoration-secondary">
@@ -11,9 +14,21 @@ export default function Home() {
         <p className="font-mainText text-accent">
           I am a full-stack developer with 2 years in frontend and 1 year in
           backend development. You&apos;ll learn about me, my skills, and see
-          some examples of my work. You can also reach out via a contact form
-          and view my resume!
+          some examples of my work. You can also reach out via the contact form
+          below and view my resume!
         </p>
+        <div className="mt-6 flex flex-row gap-x-6 justify-center">
+          <Link to="/contact">
+            <button className="font-mainText text-accent2 text-2xl lg:text-3xl p-4 bg-primary rounded-xl shadow-lg transition duration-300 ease-in hover:-translate-y-1 hover:scale-110">
+              Contact
+            </button>
+          </Link>
+          <Link to={resume} target="_blank" rel="noopener noreferrer">
+            <button className="font-mainText text-accent2 text-2xl lg:text-3xl p-4 bg-primary rounded-xl shadow-lg transition duration-300 ease-in hover:-translate-y-1 hover:scale-110">
+              Resume
+            </button>
+          </Link>
+        </div>
       </div>
     </section>
   );

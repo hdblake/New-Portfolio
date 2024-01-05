4752b491eaecad794135f6fed1da83f40778d8c9
= <=>
2024-01-05 14:58:09 -0600

    added colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    colors: {},
+    fontFamily: {
+      header: ["Ubuntu", "sans-serif"],
+      mainText: ["Prompt", "sans-serif"],
+    },
+    colors: {
+      primary: "#f57701",
+      secondary: "#017ff5",
+      accent: "#000",
+    },
     extend: {},
   },
   variants: {},

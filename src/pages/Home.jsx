d1918dd15ef3595324f2a4ed2a797a6ca77dccc3
= <=>
2024-01-05 20:34:19 -0600

    updated styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,21 @@ import resume from "../images/Resume.pdf";
 
 export default function Home() {
   return (
-    <section className="p-4">
+    <section className="p-4 absolute top-1/3">
       <h1 className="text-[42px] md:text-5xl font-header text-accent text-center md:text-left">
         Hi, I&apos;m{" "}
         <span className="text-primary underline decoration-4 decoration-secondary">
           Hunter Blake!
         </span>
       </h1>
-      <div className="w-full md:w-1/2 mt-2 text-md md:text-lg text-center md:text-left">
+      <div className="w-full md:w-2/5 mt-2 text-md md:text-lg text-center md:text-left">
         <p className="font-mainText text-accent">
           I am a full-stack developer with 2 years in frontend and 1 year in
           backend development. You&apos;ll learn about me, my skills, and see
           some examples of my work. You can also reach out via the contact form
           below and view my resume!
         </p>
-        <div className="mt-6 flex flex-row gap-x-6 justify-center">
+        <div className="mt-8 flex flex-row gap-x-6 lg:gap-x-12 justify-center">
           <Link to="/contact">
             <button className="font-mainText text-accent2 text-2xl lg:text-3xl p-4 bg-primary rounded-xl shadow-lg transition duration-300 ease-in hover:-translate-y-1 hover:scale-110">
               Contact

cd114c829d4854b2021bd65158c2f66bbb09f234
= <=>
2024-01-08 12:55:37 -0600

    added skills

---
+++ src/data/skills.js
@@ -0,0 +1,18 @@
+export const skills = [
+  {
+    title: "Languages",
+    content: "HTML/CSS, JavaScript, PHP, Python, SQL",
+  },
+  {
+    title: "Frameworks",
+    content: "React, Tailwind, Bootstrap, ExpressJS",
+  },
+  {
+    title: "Databases",
+    content: "MySQL, Microsoft SQL Server, MongoDB",
+  },
+  {
+    title: "Hosting",
+    content: "Netlify, Vercel, Github Pages, Render",
+  },
+];

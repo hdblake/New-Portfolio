2512560a51734535ccd5f63d7af466fbb3a4d591
= <=>
2024-01-09 16:39:58 -0600

    created projects data

---
+++ src/data/projects.js
@@ -0,0 +1,35 @@
+import electrons from "../images/electrons.png";
+import chamber from "../images/chamber.png";
+import sleep from "../images/sleepoutside.png";
+import macxmac from "../images/macxmac.png";
+
+export const projects = [
+  {
+    title: "Mac X Mac",
+    skills: "MERN Stack",
+    link: "https://macxmac.netlify.app/",
+    repo: "https://github.com/hdblake/Mac-X-Mac",
+    image: macxmac,
+  },
+  {
+    title: "Electrons",
+    skills: "React and Tailwind",
+    link: "https://electrons.vercel.app/",
+    repo: "https://github.com/hdblake/Electrons-website",
+    image: electrons,
+  },
+  {
+    title: "Sleep Outside",
+    skills: "HTML, CSS, JavaScript",
+    link: "https://sleep-outside-t10.netlify.app/",
+    repo: "https://github.com/hdblake/sleepoutside-team10",
+    image: sleep,
+  },
+  {
+    title: "Chamber of Commerce",
+    skills: "HTML, CSS, JavaScript",
+    link: "https://hdblake.github.io/Chamber-of-Commerce/",
+    repo: "https://github.com/hdblake/Chamber-of-Commerce",
+    image: chamber,
+  },
+];

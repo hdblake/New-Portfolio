8665e4d1b2bc8f576159f73e5207be4b853837f0
= <=>
2024-01-09 16:22:28 -0600

    updated styling

--- src/components/SkillsAccordion.jsx
+++ src/components/SkillsAccordion.jsx
@@ -9,25 +9,30 @@ export default function SkillsAccordion({ skills }) {
     setOpenIndex(openIndex === index ? null : index);
   };
 
+  // Change style of div to match education card
+
   return (
     <div className="mt-12 flex flex-col gap-y-8 w-4/5 m-auto lg:m-left">
       {skills.map((skill, index) => (
-        <div key={index} className="bg-primary rounded-xl p-2">
+        <div
+          key={index}
+          className="bg-primary rounded-xl p-2 border border-2 border-primary"
+        >
           <div
-            className={`text-xl md:text-2xl flex flex-row justify-between items-center ${
+            className={`flex flex-row justify-between items-center ${
               openIndex === index ? "active" : ""
             }`}
             onClick={() => handleClick(index)}
           >
-            <div>{skill.title}</div>
+            <h2 className="text-2xl md:text-3xl">{skill.title}</h2>
             <div>
               <IoIosArrowDropdownCircle color="black" fontSize={28} />
             </div>
           </div>
           {openIndex === index && (
-            <div className="bg-accent2 p-4 mt-4 rounded-xl font-mainText">
+            <p className="bg-accent2 p-4 mt-4 rounded-b-lg font-mainText text-lg md:text-xl">
               {skill.content}
-            </div>
+            </p>
           )}
         </div>
       ))}

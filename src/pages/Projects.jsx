cd5194ccc54ccb6da658969a3d9fda62e84ec955
= <=>
2024-01-09 16:58:42 -0600

    added styling and projectcard

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,13 @@
+import ProjectsCard from "../components/ProjectsCard.jsx";
+import { projects } from "../data/projects.js";
+
 export default function Projects() {
   return (
-    <section>
-      <h1>Projects</h1>
+    <section className="p-4">
+      <h1 className="text-[42px] md:text-5xl font-header text-accent text-center">
+        Projects
+      </h1>
+      <ProjectsCard projects={projects} />
     </section>
   );
 }

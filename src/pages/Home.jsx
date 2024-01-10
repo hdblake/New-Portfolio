97ca0eb7addb8db93ed982c67dcad8d8edfa7f03
= <=>
2024-01-10 14:48:40 -0600

    added styling to page with img included

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import resume from "../images/Resume.pdf";
+import selfie from "../images/me.jpg";
 import { useEffect } from "react";
 
 export default function Home() {
@@ -9,30 +10,41 @@ export default function Home() {
 
   return (
     <section className="p-4 absolute top-1/3">
-      <h1 className="text-[42px] md:text-5xl font-header text-accent text-center md:text-left">
+      <h1 className="text-[42px] md:text-5xl font-header text-accent text-center lg:text-left">
         Hi, I&apos;m{" "}
         <span className="text-primary underline decoration-4 decoration-secondary">
           Hunter Blake!
         </span>
       </h1>
-      <div className="w-full md:w-2/5 mt-2 text-md md:text-lg text-center md:text-left">
-        <p className="font-mainText text-accent">
-          I am a full-stack developer with 2 years in frontend and 1 year in
-          backend development. You&apos;ll learn about me, my skills, and see
-          some examples of my work. You can also reach out via the contact form
-          below and view my resume!
-        </p>
-        <div className="mt-8 flex flex-row gap-x-6 lg:gap-x-12 justify-center">
-          <Link to="/contact">
-            <button className="font-mainText text-accent text-2xl lg:text-3xl p-4 bg-primary rounded-xl shadow-lg transition duration-300 ease-in hover:-translate-y-1 hover:scale-110">
-              Contact
-            </button>
-          </Link>
-          <Link to={resume} target="_blank" rel="noopener noreferrer">
-            <button className="font-mainText text-accent text-2xl lg:text-3xl p-4 bg-primary rounded-xl shadow-lg transition duration-300 ease-in hover:-translate-y-1 hover:scale-110">
-              Resume
-            </button>
-          </Link>
+      <div className="flex flex-col justify-center lg:flex-row-reverse lg:justify-between">
+        <div className="mt-8 lg:-mt-12 lg:mr-24 xl:mr-32 w-auto flex justify-center">
+          <img
+            src={selfie}
+            alt="Photo of myself"
+            width={351}
+            height={351}
+            className="rounded-full"
+          />
+        </div>
+        <div className="w-full lg:w-2/5 mt-2 text-md md:text-lg text-center lg:text-left">
+          <p className="font-mainText text-accent">
+            I am a full-stack developer with 2 years in frontend and 1 year in
+            backend development. You&apos;ll learn about me, my skills, and see
+            some examples of my work. You can also reach out via the contact
+            form below and view my resume!
+          </p>
+          <div className="mt-8 flex flex-row gap-x-6 lg:gap-x-12 justify-center">
+            <Link to="/contact">
+              <button className="font-mainText text-accent text-2xl lg:text-3xl p-4 bg-primary rounded-xl shadow-lg transition duration-300 ease-in hover:-translate-y-1 hover:scale-110">
+                Contact
+              </button>
+            </Link>
+            <Link to={resume} target="_blank" rel="noopener noreferrer">
+              <button className="font-mainText text-accent text-2xl lg:text-3xl p-4 bg-primary rounded-xl shadow-lg transition duration-300 ease-in hover:-translate-y-1 hover:scale-110">
+                Resume
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
